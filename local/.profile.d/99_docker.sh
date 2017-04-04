48e156f79713b890a43c319bf4553106b26b86f4
kballou <kballou@devnulllabs.io>
2017-04-04 16:51:04 -0600

    Add "tags" to docker profile script

--- local/.profile.d/99_docker.sh
+++ local/.profile.d/99_docker.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 docker() {
-    case $@ in
+    case $1 in
         clean)
             ${HOME}/.local/bin/clean_docker
             ;;
         reap-volumes)
             ${HOME}/.local/bin/reap_docker_volumes
+            ;;
+        tags)
+            shift
+            ${HOME}/.local/bin/docker-tags $@
             ;;
         *)
             command docker "$@"
