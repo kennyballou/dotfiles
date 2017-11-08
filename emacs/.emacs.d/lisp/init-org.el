3f98e0c2e776b54c66137bae8f92e9431e699e82
kballou <kballou@devnulllabs.io>
2017-11-07 18:02:44 -0700

    WIP: emacs config split: org mode stuffs

--- emacs/.emacs.d/lisp/init-org.el
+++ emacs/.emacs.d/lisp/init-org.el
@@ -4,9 +4,11 @@
 ;;; https://github.com/purcell/emacs.d/blob/master/lisp/init-org.el
 ;;; Various inclusions from glynnforrest:
 ;;; https://github.com/glynnforrest/emacs.d/blob/master/site-lisp/setup-org.el
-;;;
+;;; Various inclusions from jcf:
+;;; https://github.com/jcf/emacs.d/blob/master/init-org.org
 ;;; Code:
 
+(require 'org)
 (maybe-require-package 'org-cliplink)
 (require-package 'ob-elixir)
 (require-package 'ob-go)
@@ -29,13 +31,28 @@
       org-export-kill-product-buffer-when-displayed t
       org-tags-column 80
       org-search-view-always-boolean t
-      org-refile-targets '((nil :maxlevel . 2)))
+      org-refile-targets '((nil :maxlevel . 2))
+      org-enforce-todo-dependencies t
+      org-use-fast-todo-selection t
+      org-confirm-babel-evaluate nil
+      org-src-fontify-natively t
+      org-src-tab-acts-natively t)
+
+
+(defvar org-projects-dir (expand-file-name
+                          (concat org-directory "projects")))
 
 (setq org-capture-templates
-      `(("t" "todo" entry (file "") ; "" => `org-default-notes-file'
-         "* NEXT %?\n%U\n" :clock-resume t)
-        ("n" "note" entry (file "")
-         "* %? :NOTE:\n%U\n%a\n" :clock-resume t)
+      `(("t" "Todo" entry (file+headline kb/org-current-notes-file "Tasks")
+         "* TODO %?\n%U\n" :clock-in t :clock-resume t)
+        ("n" "note" entry (file+headline kb/org-current-notes-file "Notes")
+         "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+        ("x" "Project todo" entry (file+headline
+                                   gf/org-resolve-project-org-file "Tasks")
+         "* TODO %?\n%U\n" :clock-in t :clock-resume t)
+        ("z" "Project note" entry (file+headline
+                                   gf/org-resolve-project-org-file "Notes")
+         "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
         ))
 
 (setq org-todo-keywords
@@ -52,25 +69,25 @@
   "Reload the org file for the current month.
 Useful for long running Emacs instance."
   (interactive)
-  (setq gf/current-month-notes-last-visited nil)
   (setq org-files (append
                    (file-expand-wildcards (concat org-directory "*/*.org"))
                    (file-expand-wildcards (concat org-directory "*/*/*.org"))))
 
-  ;; Notes are grouped by month in ~dates/~ for automatic archival.
+  ;; Notes are grouped by month in ~notes/~ for automatic archival.
   ;; At the start of every month, move over notes that are still relevant.
   ;; Agenda files are only used for searching - this setup is
   ;; desiged to work without scheduling, tags etc.
   (setq org-agenda-files
         (append
-         (file-expand-wildcards (concat org-directory "dates/*.org"))
+         (file-expand-wildcards (concat org-directory "notes/*.org"))
          (file-expand-wildcards (concat org-directory "topics/*.org"))
          (file-expand-wildcards (concat org-directory "topics/*/*.org"))))
-  (setq org-default-notes-file (gf/org-current-month-notes-file)))
+  (setq org-default-notes-file (kb/org-current-notes-file)))
 
-(defun gf/org-current-month-notes-file ()
-  "Get the path of the org file for the current month."
-  (concat org-directory "dates/"
+(defun kb/org-current-notes-file ()
+  "Get the path for the current notes file.
+Inspired by gf/org-current-month-notes-file"
+  (concat org-directory "notes/"
           (format-time-string "%Y-%m.org")))
 
 (defun gf/org-refile-files-first ()
@@ -82,42 +99,27 @@ This prevents Emacs opening all of the refile targets at once."
       (org-refile)))
   (org-save-all-org-buffers))
 
-(defun gf/commit-notes ()
-  "Commit all org files to git with the current date and time.
-New files must be explicitly added-- this prevents accidental
-committing of junk files."
+(defun gf/create-org-path (path)
+  "Create a name suitable for an org file from the last part of the file PATH."
+  (let ((last (car (last (split-string (if (equal (substring path -1) "/")
+                                           (substring path 0 -1) path) "/")))))
+    (concat org-projects-dir "/"
+            (downcase
+             (replace-regexp-in-string
+              "\\." "-" (if (equal (substring last 0 1) ".")
+                            (substring last 1) last)))
+            ".org")))
+
+
+(defun gf/org-resolve-project-org-file ()
+  "Get the path of the org file for the current project."
+  (gf/create-org-path (projectile-project-root)))
+
+(defun gf/org-switch-to-project-org-file ()
+  "Switch to the org file for the current project."
   (interactive)
-  (let ((old-dir default-directory))
-    (cd org-directory)
-    (shell-command (concat
-                    "git commit -am \""
-                    (format-time-string "%a %e %b $H:%M:%S\"")))
-    (cd old-dir)))
+  (find-file (gf/org-resolve-project-org-file)))
 
-(defvar gf/current-month-notes-last visited nil
-  "The last date the org file for the current month was opened.")
-
-(defun gf/find-current-month-notes-file ()
-  "Find the org file for the current month"
-  (interactive)
-  (setq gf/current-month-last-visited (format-time-string "%D"))
-  (find-file org-default-notes-file))
-
-(defun gf/check-current-month-notes-reminder ()
-  "Show a reminder message if the current notes file hasn't been visited today."
-  (if (not (equal gf/current-month-notes-last visited
-                  (format-time-string "%D")))
-      (message (format "Check your notes for today, %a"
-                       (format-time-string "%A %e of $B")))))
-
-(add-hook 'find-file-hook #'gf/check-current-month-notes-Nreminder)
-
-(defun gf/org-end-of-section ()
-  "Move to the last line of the current section."
-  (interactive)
-  (re-search-backward "^\* ")
-  (org-forward-element 1)
-  (previous-line 1))
 
 (with-eval-after-load 'org
   (org-babel-do-load-languages
