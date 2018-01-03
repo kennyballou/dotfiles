0a077fea733061c40ad95d6795cc30378dfafb17
kballou <kballou@devnulllabs.io>
2018-01-03 15:40:45 -0700

    wip: emacs config split: add docker-tramp

--- emacs/.emacs.d/lisp/init-docker.el
+++ emacs/.emacs.d/lisp/init-docker.el
@@ -5,9 +5,11 @@
 (defvar docker)
 (defvar dockerfile-mode)
 (defvar docker-compose-mode)
+(defvar docker-tramp)
 (use-package docker)
 (use-package dockerfile-mode)
 (use-package docker-compose-mode)
+(use-package docker-tramp)
 
 (provide 'init-docker)
 ;;; init-docker.el ends here
