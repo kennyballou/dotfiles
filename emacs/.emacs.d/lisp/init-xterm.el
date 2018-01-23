21ebc55d5f266e097056259af0b1da932e120431
Kenny Ballou <kballou@devnulllabs.io>
2018-01-23 14:21:19 -0700

    wip: emacs config split: fix flycheck errors

--- emacs/.emacs.d/lisp/init-xterm.el
+++ emacs/.emacs.d/lisp/init-xterm.el
@@ -1,3 +1,7 @@
+;;; init-xterm --- xterm configuration
+;;; Commentary:
+;;; Code:
+
 (require 'init-frame-hooks)
 
 (global-set-key [mouse-4] (lambda () (interactive) (scroll-down 1)))
@@ -6,9 +10,11 @@
 (autoload 'mwheel-install "mwheel")
 
 (defun sanityinc/console-frame-setup ()
+  "Configure console frame buffer."
   (xterm-mouse-mode 1)
   (mwheel-install))
 
 (add-hook 'after-make-console-frame-hooks 'sanityinc/console-frame-setup)
 
 (provide 'init-xterm)
+;;; init-xterm.el ends here
