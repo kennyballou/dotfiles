f29cc917fab82ea8d0cf7baf43e47e710d728937
Kenny Ballou <kballou@devnulllabs.io>
2018-01-31 12:59:20 -0700

    wip: emacs config split: update visual commands, again

--- emacs/.emacs.d/lisp/init-eshell.el
+++ emacs/.emacs.d/lisp/init-eshell.el
@@ -104,7 +104,11 @@ left to try and get the PATH down to, at most, MAX-LEN."
 (setq eshell-highlight-prompt nil)
 (setq eshell-history-size 4096)
 (setq eshell-prompt-regexp "(.*)(.*)\\(\\[.*\\]\\)?[%±] ")
-(dolist (cmd '("vim" "yarn" "composer" "pip"))
+(dolist (cmd '("vim"
+               "yarn"
+               "composer"
+               "pip"
+               "qemu-system-x86_64"))
   (add-to-list 'eshell-visual-commands cmd))
 (dolist (subcmd '(("git" "log" "diff" "show")
                   ("docker" "build" "run" "exec" "logs")
