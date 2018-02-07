3c3ba94d1b57cefa6357c4896e8bab1ba026262a
Kenny Ballou <kballou@devnulllabs.io>
2018-02-07 11:08:10 -0700

    wip: emacs config split: python changes

--- emacs/.emacs.d/lisp/init-python.el
+++ emacs/.emacs.d/lisp/init-python.el
@@ -2,43 +2,46 @@
 ;;; Commentary:
 ;;; Code:
 
+(defvar python)
 (defvar virtualenvwrapper)
 (defvar venv-dirlookup-names)
 (defvar venv-location)
+(defvar pip-requirements)
 (defvar python-shell-interpreter)
 (defvar python-shell-interpreter-args)
+(defvar flycheck-disabled-checkers)
+(defvar anaconda-mode)
+(defvar anaconda-eldoc-mode)
 
-(add-to-list 'auto-mode-alist '("SConstruct\\'" . 'python-mode))
-(add-to-list 'auto-mode-alist '("SConscript\\'" . 'python-mode))
-
-(require-package 'pip-requirements)
-(use-package virtualenvwrapper
-  :ensure t
+(use-package python
   :init
-  (setq venv-dirlookup-names '(".venv" "pyenv" ".virtual"))
-  (setq venv-location (expand-file-name "~/.venvs"))
+  (when (executable-find "ipython")
+    (setq python-shell-interpreter "ipython"
+          python-shell-interpreter-args "--profile=emacs"))
   :config
+  (use-package pip-requirements)
+  (use-package virtualenvwrapper
+    :ensure t
+    :init
+    (setq venv-dirlookup-names '(".venv" "pyenv" ".virtual"))
+    (setq venv-location (expand-file-name "~/.venvs"))
+    :config
 
-  (venv-initialize-interactive-shells)
+    (venv-initialize-interactive-shells)
 
+    )
   )
 
-(when (maybe-require-package 'anaconda-mode)
-  (with-eval-after-load 'python
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode)))
-
-(defvar flycheck-disabled-checkers)
+(use-package anaconda-mode
+  :after python
+  :hook ((python-mode . anaconda-mode)
+         (python-mode . anaconda-eldoc-mode)))
 
 (when (maybe-require-package 'flycheck)
   (with-eval-after-load 'flycheck
     (add-to-list 'flycheck-disabled-checkers 'python-pylint)
     (add-hook 'python-mode-hook
               (lambda () (flycheck-select-checker 'python-flake8)))))
-
-(when (executable-find "ipython")
-  (setq python-shell-interpreter "ipython"
-        python-shell-interpreter-args "--profile=emacs"))
 
 (provide 'init-python)
 ;;; init-python.el ends here
