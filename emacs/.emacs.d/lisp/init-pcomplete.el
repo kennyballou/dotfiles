a9f0a7d677c1b3bc57492403c41f5b100bd2187d
Kenny Ballou <kballou@devnulllabs.io>
2018-02-20 08:51:44 -0700

    wip: emacs config split: use pcomplete packages

--- emacs/.emacs.d/lisp/init-pcomplete.el
+++ emacs/.emacs.d/lisp/init-pcomplete.el
@@ -2,39 +2,18 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Git Commands
-;; https://www.masteringemacs.org/article/pcomplete-context-sensitive-completion-emacs
-(defconst pcmpl-git-commands
-  '("add" "bisect" "branch" "checkout" "clone" "commit"
-    "diff" "fetch" "grep" "init" "log" "merge" "mv" "pull"
-    "push" "rebase" "reset" "rm" "show" "status" "tag"
-    "send-email" "format-patch" "am" "apply" "cherry-pick")
-  "List of `git' commands.")
-
-(defvar pcmpl-git-ref-list-cmd "git for-each-ref refs/ --format='%(refname)'"
-  "The `git' command to run to get a list of refs.")
-
-(defun pcmpl-git-get-refs (type)
-  "Return a list of `git' refs filtered by TYPE."
-  (with-temp-buffer
-    (insert (shell-command-to-string pcmpl-git-ref-list-cmd))
-    (goto-char (point-min))
-    (let ((ref-list))
-      (while (re-search-forward (concat "^refs/" type "/\\(.+\\)$") nil t)
-        (add-to-list 'ref-list (match-string 1)))
-      ref-list)))
-
-(defun pcomplete/git ()
-  "Completion for `git'."
-  ;; Completion for the command argument.
-  (pcomplete-here* pcmpl-git-commands)
-  (cond
-  ;; complete files/directories forever if the command is `add' or `rm'.
-  ((pcomplete-match (regexp-opt '("add" "rm")) 1)
-   (while (pcomplete-here (pcomplete-entries))))
-  ;; provide branch completion for the command `checkout' and `cherry-pick'.
-  ((or (pcomplete-match "checkout" 1) (pcomplete-match "cherry-pick" 1))
-   (pcomplete-here* (pcmpl-git-get-refs "heads")))))
+(defvar pcmpl-args)
+(defvar pcmpl-git)
+(defvar pcmpl-pip)
+(defvar pcomplete-extension)
+(defvar melpa-unstable)
+(use-package pcmpl-args
+  :pin melpa-unstable)
+(use-package pcmpl-git
+  :pin melpa-unstable)
+(use-package pcmpl-pip
+  :pin melpa-unstable)
+(use-package pcomplete-extension)
 
 
 (provide 'init-pcomplete)
