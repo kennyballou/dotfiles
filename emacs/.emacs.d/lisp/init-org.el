97a01b64cf5dcbe4ad8243664fcfafec1b15bb1c
Kenny Ballou <kballou@devnulllabs.io>
2018-07-24 14:42:34 -0600

    wip: emacs config split: set org ellipsis character

--- emacs/.emacs.d/lisp/init-org.el
+++ emacs/.emacs.d/lisp/init-org.el
@@ -32,6 +32,7 @@
 ;; Various preferences
 (setq org-log-done t
       org-directory "~/.org/"
+      org-ellipsis "⤵"
       org-edit-timestamp-down-means-later t
       org-archive-mark-done nil
       org-hide-emphasis-markers t
