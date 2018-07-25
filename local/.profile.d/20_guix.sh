822331b02caf4836e082fa6320649524f6224ec6
Kenny Ballou <kballou@devnulllabs.io>
2018-07-25 16:47:55 -0600

    profile.d: add guix environment variables

---
+++ local/.profile.d/20_guix.sh
@@ -0,0 +1,6 @@
+GUIX_PROFILE="${HOME}/.guix-profile/"
+export PATH="${GUIX_PROFILE}/bin:${GUIX_PROFILE}/sbin${PATH:+:}${PATH}"
+export SSL_CERT_DIR="${GUIX_PROFILE}etc/ssl/certs"
+export SSL_CERT_FILE="${GUIX_PROFILE}/etc/ssl/certs/ca-certificates.crt"
+export GIT_SSL_CAINFO="${SSL_CERT_FILE}"
+export GUIX_LOCPATH="${GUIX_PROFILE}/lib/locale"
