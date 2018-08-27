be00792143c2a083ac78f65e1a64fdc77a560dd1
Kenny Ballou <kballou@devnulllabs.io>
2018-08-27 15:45:40 -0600

    wip: emacs: htmlize customizations
    disable fci-mode and flyspell when creating html exports/files

--- emacs/.emacs.d/lisp/init-org.el
+++ emacs/.emacs.d/lisp/init-org.el
@@ -31,6 +31,7 @@
 (defvar ox-gfm)
 (defvar org-babel-eval-in-repl)
 (defvar htmlize)
+(defvar fci-mode)
 
 (require 'org)
 (require 'ox-md)
@@ -47,7 +48,54 @@
   :pin melpa-unstable)
 (use-package ox-gfm)
 (use-package org-babel-eval-in-repl)
-(use-package htmlize)
+;; https://www.reddit.com/r/emacs/comments/57gteu/org_export_html_source_code_blocks_adds_trailing/
+;; https://github.com/kaushalmodi/.emacs.d/blob/15df5b075a4cd4a8ed7332237c0e33e75b430a36/setup-files/setup-htmlize.el#L10-L28
+(use-package htmlize
+  :defer t
+  :config
+  (progn
+
+    ;; It is required to disable `fci-mode' when `htmlize-buffer' is called;
+    ;; otherwise the invisible fci characters show up as funky looking
+    ;; visible characters in the source code blocks in the html file.
+    ;; http://lists.gnu.org/archive/html/emacs-orgmode/2014-09/msg00777.html
+    (with-eval-after-load 'fill-column-indicator
+      (defvar modi/htmlize-initial-fci-state nil
+        "Variable to store state of `fci-mode' when `htmlize-buffer' is called.")
+
+      (defun modi/htmlize-before-hook-fci-disable ()
+        (setq modi/htmlize-initial-fci-state fci-mode)
+        (when fci-mode
+          (fci-mode -1)))
+
+      (defun modi/htmlize-after-hook-fci-enable-maybe ()
+        (when modi/htmlize-initial-fci-state
+          (setq fci-mode 1)))
+
+      (add-hook 'htmlize-before-hook #'modi/htmlize-before-hook-fci-disable)
+      (add-hook 'htmlize-after-hook #'modi/htmlize-after-hook-fci-enable-maybe))
+
+    ;; `flyspell-mode' also has to be disabled because depending on the
+    ;; theme, the squiggly underlines can either show up in the html file
+    ;; or cause elisp errors like:
+    ;; (wrong-type-argument number-or-marker-p (nil . 100))
+    (with-eval-after-load 'flyspell
+      (defvar modi/htmlize-initial-flyspell-mode nil
+        "Variable to store state of `flyspell-mode' when
+`htmlize-buffer` is called.")
+
+      (defun modi/htmlize-before-hook-flyspell-disable ()
+        (setq modi/htmlize-initial-flyspell-mode flyspell-mode)
+        (when flyspell-mode
+          (setq flyspell-mode -1)))
+
+      (defun modi/htmlize-after-hook-flyspell-enable-maybe ()
+        (when modi/htmlize-initial-flyspell-mode
+          (setq flyspell-mode 1)))
+
+      (add-hook 'htmlize-before-hook #'modi/htmlize-before-hook-flyspell-disable)
+      (add-hook 'htmlize-after-hook #'modi/htmlize-after-hook-flyspell-enable-maybe))))
+
 (use-package ob-ipython
   :pin melpa-unstable
   :defer t)
