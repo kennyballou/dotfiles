d7db0ac0f5d4c6e5fba9521e1a103b1faf1ef745
Kenny Ballou <kballou@devnulllabs.io>
2018-10-16 14:52:10 -0600

    emacs: new initial configuration

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,36 @@
+;;; init --- Emacs initialization file
+;;; -*- lexical-binding: t -*-
+;;; Commentary:
+;;; This file is bootstraps Emacs configuration which is
+;;; divided into a number of separate files
+;;; based on: https://github.com/purcell/emacs.d/blob/master/init.el
+;;; Takes inspiration from https://github.com/jaypei/emacs.d
+;;; and many others.
+;;; Code:
+
+(let ((minver "24.1"))
+  (when (version< emacs-version minver)
+    (error "Emacs is too old, this config requires v%s or higher" minver)))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(defconst *spell-check-support-enabled* t)
+
+;; Adjust Garbage Collection Settings
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'after-init-hook
+    (lambda () (setq gc-cons-threshold (* 20 1024 1024)))))
+
+;; customize interface configuration
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+(provide 'init)
+;;; init.el ends here
+
+;;; Local Variables:
+;;; coding: utf-8
+;;; no-byte-compile: t
