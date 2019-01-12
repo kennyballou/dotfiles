4bbe3869d71084cb679635d0b8c47e053ad9dce7
Kenny Ballou <kballou@devnulllabs.io>
2019-01-11 18:28:23 -0700

    emacs: ivy: reconfigure ivy, swiper, and counsel
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/lisp/init-ivy.el
+++ emacs/.emacs.d/lisp/init-ivy.el
@@ -1,43 +1,57 @@
 ;;; init-ivy --- Maybe use ivy
 ;;; -*- lexical-binding: t -*-
 ;;; Commentary:
+;;; https://truthseekers.io/lessons/how-to-use-ivy-swiper-counsel-in-emacs-for-noobs/
+;;; https://github.com/abo-abo/swiper
+;;; https://github.com/purcell/emacs.d/blob/b2aea30/lisp/init-ivy.el
 ;;; Code:
 
+(defvar ivy)
 (defvar ivy-mode)
+(defvar swiper)
+(defvar counsel)
 (defvar projectile-completion-system)
 (defvar magit-completing-read-function)
 (defvar dumb-jump-selector)
 (defvar rtags-display-result-backend)
+(defvar ivy-use-virtual-buffers)
+(defvar ivy-virtual-abbreviate)
+(defvar ivy-count-format)
+(defvar ivy-initial-inputs-alist)
+(defvar counsel-mode-override-describe-bindings)
 
 (use-package ivy
-  :commands ivy-mode
-  :init
-  (require 'ivy nil t)
-  (setq-default ivy-use-virtual-buffers t
-                ivy-virtual-abbreviate 'fullpath
-                ivy-count-format "%d/%d: "
-                ivy-initial-inputs-alist)
-  (setq projectile-completion-system 'ivy
-        magit-completing-read-function 'ivy-completing-read
-        dumb-jump-selector 'ivy
-        rtags-display-result-backend 'ivy)
-  :config (ivy-mode 1)
-  :hook (after-init-hook . ivy-mode)
+  :delight)
+(use-package swiper
+  :delight)
+(use-package counsel
   :delight)
 
-(use-package counsel
-  :delight
-  :init
-  (setq-default consul-mode-override-describe-bindings t))
+(ivy-mode 1)
 
-(use-package swiper
-  :functions sanityinc/swiper-at-point
-  :bind (("M-s /" . #'sanityinc/swiper-at-point))
-  :init
-  (defun sanityinc/swiper-at-point (sym)
-    "Use `swiper' to search for a the symbol at point."
-    (interactive (list (thing-at-point 'symbol)))
-    (swiper sym)))
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(setq ivy-virtual-abbreviate 'fullpath)
+(setq ivy-count-format "%d/%d: ")
+(setq ivy-initial-inputs-alist nil)
+(setq counsel-mode-override-describe-bindings t)
+
+(defun sanityinc/swiper-at-point (sym)
+  "Use `swiper' to search for a the symbol SYM at point."
+  (interactive (list (thing-at-point 'symbol)))
+  (swiper sym))
+
+(global-set-key (kbd "M-x") #'counsel-M-x)
+(global-set-key (kbd "C-s") #'swiper)
+(global-set-key (kbd "M-s /") #'sanityinc/swiper-at-point)
+(global-set-key (kbd "C-x C-f") #'counsel-find-file)
+(global-set-key (kbd "C-h f") #'counsel-describe-function)
+(global-set-key (kbd "C-h v") #'counsel-describe-variable)
+(global-set-key (kbd "C-c C-r") #'ivy-resume)
+
+(global-set-key (kbd "C-c g") #'counsel-git-grep)
+(global-set-key (kbd "C-c l") #'counsel-locate)
+
 
 (provide 'init-ivy)
 ;;; init-ivy.el ends here
