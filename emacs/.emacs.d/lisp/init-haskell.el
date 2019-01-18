4b23f0d19aea488bd399a9b05824499ecde95058
Kenny Ballou <kballou@devnulllabs.io>
2019-01-18 06:38:09 -0700

    emacs: haskell (re)configurations
    Remove `intero` mode for vanilla `haskell-mode`.
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/lisp/init-haskell.el
+++ emacs/.emacs.d/lisp/init-haskell.el
@@ -3,26 +3,17 @@
 ;;; Code:
 
 (defvar haskell-mode)
-(defvar intero)
 (defvar flycheck)
+(defvar flycheck-haskell)
 (defvar intero-mode-map)
 (defvar hindent)
 
 (use-package hindent
   :defer t)
 
-(use-package intero
-  :defer
-  :after flycheck
-  :config
-  (unbind-key (kbd "M-?") intero-mode-map)
-  (flycheck-add-next-checker 'intero
-                             '(warning . haskell-hlint)))
-
 (use-package haskell-mode
-  :after (intero hindent flycheck)
+  :after (hindent flycheck)
   :hook ((haskell-mode-hook . eldoc-mode))
-  :interpreter ("ghci" . haskell-mode)
   :bind (:map interactive-haskell-mode-map
               ("M-N" . haskell-goto-next-error)
               ("M-P" . haskell-goto-prev-error)
@@ -30,9 +21,10 @@
               ("C-c h" . hoogle)
               ("C-o" . open-line))
   :init
-  (setq-default haskell-stylish-on-save t)
-  :config
-  (intero-global-mode))
+  (setq-default haskell-stylish-on-save t))
+
+(use-package flycheck-haskell
+  :after (haskell-mode flycheck))
 
 (provide 'init-haskell)
 ;;; init-haskell.el ends here
