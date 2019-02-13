4115bf06b3344d4f96999cd0ff5bb5c6ff09a29d
Kenny Ballou <kballou@devnulllabs.io>
2019-02-13 11:33:30 -0700

    emacs: misc: add hook to close minibuffer on mouse
    When mouse leave events, close the minibuffer.  Need to add another such
    entry if exiting the minibuffer for any reason.
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/lisp/init-misc.el
+++ emacs/.emacs.d/lisp/init-misc.el
@@ -35,5 +35,13 @@
           (setq split-height-threshold 160)
         (setq split-height-threshold 80))))
 
+;; https://trey-jackson.blogspot.com/2010/04/emacs-tip-36-abort-minibuffer-when.html
+(defun stop-using-minibuffer ()
+  "Kill the minibuffer."
+  (when (and (>= (recursion-depth) 1) (active-minibuffer-window))
+    (abort-recursive-edit)))
+
+(add-hook 'mouse-leave-buffer-hook #'stop-using-minibuffer)
+
 (provide 'init-misc)
 ;;; init-misc.el ends here
