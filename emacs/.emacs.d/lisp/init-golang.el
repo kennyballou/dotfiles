096569122afc130d997e75d6deeb320ebab32b11
Kenny Ballou <kballou@devnulllabs.io>
2019-09-23 15:55:06 -0600

    emacs: go: add eldoc and stacktracer
    [0]: https://github.com/abrochard/emacs-config/blob/master/configuration.org#go
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/lisp/init-golang.el
+++ emacs/.emacs.d/lisp/init-golang.el
@@ -3,6 +3,8 @@
 ;;; Code:
 
 (defvar go-mode)
+(defvar go-eldoc)
+(defvar go-stacktracer)
 (defvar go-autocomplete)
 (defvar before-save-hook)
 
@@ -17,10 +19,21 @@
   :commands (gofmt-before-save)
   :defer t
   :config
+  (setq auto-mode-alist
+      (append '(("go.mod" . text-mode)) auto-mode-alist))
   (add-hook 'go-mode-hook (lambda ()
                             (setq indent-tabs-mode t)
                             (setq tab-width 4)))
   (add-hook 'before-save-hook #'gofmt-before-save))
+
+(use-package go-eldoc
+  :after (go-mode)
+  :commands (go-eldoc-setup)
+  :diminish eldoc-mode
+  :config
+  (add-hook 'go-mode-hook #'go-eldoc-setup))
+
+(use-package go-stacktracer)
 
 (provide 'init-golang)
 ;;; init-golang.el ends here
