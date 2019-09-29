aa4fbe72faf06a7aafac64fb844ec35467deefb4
Kenny Ballou <kballou@devnulllabs.io>
2019-09-29 12:49:44 -0600

    emacs: elm: override reactor arguments
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/lisp/init-elm.el
+++ emacs/.emacs.d/lisp/init-elm.el
@@ -5,6 +5,7 @@
 (defvar flycheck)
 (defvar flycheck-elm)
 (defvar elm-mode)
+(defvar elm-reactor-arguments)
 (defvar melpa-unstable)
 
 (use-package flycheck-elm
@@ -14,14 +15,13 @@
   :after (flycheck flycheck-elm)
   :defer t
   :diminish
-  :init
-  (setq-default elm-format-on-save t)
   :config
   (flycheck-elm-setup)
   (when (executable-find "elm-oracle")
     (add-hook 'elm-mode-hook 'elm-oracle-setup-completion))
   (when (executable-find "elm-format")
-    (setq-default elm-format-on-save t)))
+    (setq-default elm-format-on-save t))
+  (setq elm-reactor-arguments nil))
 
 (provide 'init-elm)
 ;;; init-elm.el ends here
