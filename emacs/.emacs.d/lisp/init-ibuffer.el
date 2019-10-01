1d6a9e7b4db9b49c9cf6550d52f8bcbc84462e29
Kenny Ballou <kballou@devnulllabs.io>
2019-10-01 15:14:52 -0600

    emacs: ibuffer: add ibuffer-vc
    Group buffers together by VC root.
    
    [0]: https://github.com/purcell/emacs.d/blob/master/lisp/init-ibuffer.el
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/lisp/init-ibuffer.el
+++ emacs/.emacs.d/lisp/init-ibuffer.el
@@ -3,11 +3,24 @@
 ;;; Code:
 
 (defvar ibuffer)
+(defvar ibuffer-vc)
 (defvar ibuffer-quit)
+(defvar ibuffer-vc-set-filter-groups-by-vc-root)
+(defvar ibuffer-sorting-mode)
 
 (use-package ibuffer
   :config
   (fullframe ibuffer ibuffer-quit))
+
+(use-package ibuffer-vc
+  :after (ibuffer)
+  :config
+  (defun ibuffer-set-up-preferred-filters ()
+    "Setup ibuffer sorting and filtering."
+    (ibuffer-vc-set-filter-groups-by-vc-root)
+    (unless (eq ibuffer-sorting-mode 'filename/process)
+      (ibuffer-do-sort-by-filename/process)))
+  (add-hook 'ibuffer-hook #'ibuffer-set-up-preferred-filters))
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
