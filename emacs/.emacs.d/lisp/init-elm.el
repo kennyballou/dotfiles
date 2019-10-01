08da9401bd8847a2421c2949f32a2511e22f4b59
Kenny Ballou <kballou@devnulllabs.io>
2019-10-01 15:16:24 -0600

    emacs: elm: diminish elm-indent minor mode
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/lisp/init-elm.el
+++ emacs/.emacs.d/lisp/init-elm.el
@@ -21,7 +21,8 @@
     (add-hook 'elm-mode-hook 'elm-oracle-setup-completion))
   (when (executable-find "elm-format")
     (setq-default elm-format-on-save t))
-  (setq elm-reactor-arguments nil))
+  (setq elm-reactor-arguments nil)
+  (diminish 'elm-indent-mode))
 
 (provide 'init-elm)
 ;;; init-elm.el ends here
