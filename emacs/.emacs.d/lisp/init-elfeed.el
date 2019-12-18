5c49096d183c9f2d9d957273604fcb365a2d84a1
Kenny Ballou <kballou@devnulllabs.io>
2019-12-18 16:49:13 -0700

    emacs: add elfeed-org
    Organize feeds using an org file!
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/lisp/init-elfeed.el
+++ emacs/.emacs.d/lisp/init-elfeed.el
@@ -3,8 +3,16 @@
 ;;; Code:
 
 (defvar elfeed)
+(defvar elfeed-org)
+(defvar rmh-elfeed-org-files)
 
 (use-package elfeed)
+
+(use-package elfeed-org
+  :after elfeed
+  :config
+  (setq rmh-elfeed-org-files (list "~/.org/elfeed.org"))
+  (elfeed-org))
 
 (provide 'init-elfeed)
 ;;; init-elfeed.el ends here
