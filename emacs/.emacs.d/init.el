5eab052ce4e0df9882b5cbc113f21a0b54644b98
Kenny Ballou <kballou@devnulllabs.io>
2020-01-21 21:03:23 -0700

    emacs: re-nable (real)gud
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -70,7 +70,7 @@
 (require 'init-vc)
 (require 'init-git)
 (require 'init-projectile)
-;; (require 'init-gud)
+(require 'init-gud)
 (require 'init-compile)
 (require 'init-regex)
 (require 'init-sessions)
