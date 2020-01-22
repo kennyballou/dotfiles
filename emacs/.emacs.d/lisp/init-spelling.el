02d93bf52b0b4600d82032a1d9402aa71fea73bc
Kenny Ballou <kballou@devnulllabs.io>
2020-01-21 22:05:53 -0700

    emacs: add some ispell arguments
    https://blog.binchen.org/posts/effective-spell-check-in-emacs.html
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/lisp/init-spelling.el
+++ emacs/.emacs.d/lisp/init-spelling.el
@@ -3,10 +3,11 @@
 ;;; Code:
 
 (defvar ispell)
-(defvar ispell-program-name)
+(defvar ispell-extra-args)
 
 (use-package ispell
-  :hook ((text-mode-hook . (lambda () flyspell-mode t))))
+  :config
+  (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together")))
 
 (provide 'init-spelling)
 ;;; init-spelling.el ends here
