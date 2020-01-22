6bbb7ac968f672255067822909bf0284990081de
Kenny Ballou <kballou@devnulllabs.io>
2020-01-21 22:06:54 -0700

    emacs: meghanada: configurate more
    There's a sample configuration in the README that is mostly copied
    here[0].
    
    [0]: https://github.com/mopemope/meghanada-emacs
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/lisp/init-java.el
+++ emacs/.emacs.d/lisp/init-java.el
@@ -2,10 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 
+(defvar autodisass-java-bytecode)
 (defvar jdee)
 (defvar meghanada)
+(defvar meghanada-server-remote-debug)
+(defvar meghanada-javac-xlint)
 (defvar melpa-unstable)
 (defvar jdee-server-dir)
+
+(use-package autodisass-java-bytecode
+  :defer t)
 
 (use-package jdee
   :defer t
@@ -16,7 +22,62 @@
                                 "/")))
 
 (use-package meghanada
-  :mode ("\\.java" . meghanada-mode))
+  :defer t
+  :commands (meghanada-mode)
+  :init
+  (add-hook 'java-mode-hook
+            (lambda ()
+              (meghanada-mode t)
+              (smartparens-mode t)
+              (rainbow-delimiters-mode t)
+              (auto-highlight-symbol-mode t)
+              (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)))
+  :config
+  (setq indent-tabs-mode nil)
+  (setq meghanada-server-remote-debug t)
+  (setq meghanada-javac-xlint "-Xlint:all,-processing")
+  :bind (:map meghanada-mode-map
+              ("C-S-t" . meghanada-switch-testcase)
+              ("M-RET" . meghanada-local-variable)
+              ("C-M-." . helm-imenu)
+              ("M-r" . meghanada-reference)
+              ("M-t" . meghanada-typeinfo)
+              ("C-z" . hydra-meghananda/body)))
+
+(defhydra hydra-meghanada (:hint nil :exit t)
+  "
+^Edit^                           ^Tast or Task^
+^^^^^^-------------------------------------------------------
+_f_: meghanada-compile-file      _m_: meghanada-restart
+_c_: meghanada-compile-project   _t_: meghanada-run-task
+_o_: meghanada-optimize-import   _j_: meghanada-run-junit-test-case
+_s_: meghanada-switch-test-case  _J_: meghanada-run-junit-class
+_v_: meghanada-local-variable    _R_: meghanada-run-junit-recent
+_i_: meghanada-import-all        _r_: meghanada-reference
+_g_: magit-status                _T_: meghanada-typeinfo
+_l_: helm-ls-git-ls
+_q_: exit
+"
+  ("f" meghanada-compile-file)
+  ("m" meghanada-restart)
+  ("c" meghanada-compile-project)
+  ("o" meghanada-optimize-import)
+  ("s" meghanada-switch-test-case)
+  ("v" meghanada-local-variable)
+  ("i" meghanada-import-all)
+
+  ("g" magit-status)
+  ("l" helm-ls-git-ls)
+  ("t" meghanada-run-task)
+  ("T" meghanada-typeinfo)
+  ("j" meghanada-run-junit-test-case)
+  ("J" meghanada-run-junit-class)
+  ("R" meghanada-run-junit-recent)
+  ("r" meghanada-reference)
+
+  ("q" exit)
+  ("z" nil "leave"))
+
 
 (provide 'init-java)
 ;;; init-java.el ends here
