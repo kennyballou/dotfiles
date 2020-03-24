b16f72557e3961a442671366e97ee6265037a16d
Kenny Ballou <kballou@devnulllabs.io>
2020-03-23 23:52:57 -0600

    emacs: yas: move snippets directory
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/lisp/init-yas.el
+++ emacs/.emacs.d/lisp/init-yas.el
@@ -6,7 +6,10 @@
 (defvar yas-snippet-dirs)
 
 (use-package yasnippet
-  :diminish (yas-minor-mode))
+  :diminish (yas-minor-mode)
+  :config
+  (setq yas-snippet-dirs
+        '("~/.emacs.d/snippets")))
 
 (provide 'init-yas)
 ;;; init-yas.el ends here
