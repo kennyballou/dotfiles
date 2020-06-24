7082814e58b7717ce156ff0f4dbfc551c1a664ac
Kenny Ballou <kballou@devnulllabs.io>
2020-06-24 16:25:18 -0600

    emacs: add unicode-fonts support
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -137,6 +137,7 @@
 
 ;; Locales (setting them earlier in this file doesn't work in X)
 (require 'init-locales)
+(require 'init-unicode)
 
 (use-package uptimes
   :hook
---
+++ emacs/.emacs.d/lisp/init-unicode.el
@@ -0,0 +1,13 @@
+;;; init-unicode --- Unicode Fonts Configuration
+;;; Commentary:
+;;; ;;; https://emacs-fu.blogspot.com/2009/08/managing-e-mail-addresses-with-bbdb.html
+;;; Code:
+
+(defvar unicode-fonts)
+
+(use-package unicode-fonts
+  :config
+  (unicode-fonts-setup))
+
+(provide 'init-unicode)
+;;; init-unicode.el ends here
