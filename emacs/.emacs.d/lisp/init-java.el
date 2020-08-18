15510e4842f8afb971dbc0e4015b44bd1b918973
Kenny Ballou <kballou@devnulllabs.io>
2020-08-18 12:39:31 -0600

    emacs: java: reconfigure to use lsp-java
    Replace all other java packages with lsp-java.
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/lisp/init-java.el
+++ emacs/.emacs.d/lisp/init-java.el
@@ -3,81 +3,61 @@
 ;;; Code:
 
 (defvar autodisass-java-bytecode)
-(defvar jdee)
 (defvar meghanada)
 (defvar meghanada-server-remote-debug)
 (defvar meghanada-javac-xlint)
-(defvar melpa-unstable)
-(defvar jdee-server-dir)
+(defvar lsp-java)
+(defvar dap-java)
 (defvar c-basic-offset)
 
 (use-package autodisass-java-bytecode
-  :defer t)
-
-(use-package jdee
   :defer t
-  :custom
-  (jdee-server-dir (string-join (list (getenv "HOME")
-                                      ".m2/repository/com/github"
-                                      "jdee-emacs/jdee-server/1.1-SNAPSHOT")
-                                "/")))
-
-(use-package meghanada
-  :defer t
-  :commands (meghanada-mode)
-  :init
-  (add-hook 'java-mode-hook
-            (lambda ()
-              (meghanada-mode t)
-              (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)))
   :config
-  (setq indent-tabs-mode nil)
-  (setq tab-width 4)
-  (setq c-basic-offset 4)
-  (setq meghanada-server-remote-debug t)
-  (setq meghanada-javac-xlint "-Xlint:all,-processing")
-  :bind (:map meghanada-mode-map
-              ("C-S-t" . meghanada-switch-testcase)
-              ("M-RET" . meghanada-local-variable)
-              ("C-M-." . helm-imenu)
-              ("M-r" . meghanada-reference)
-              ("M-t" . meghanada-typeinfo)
-              ("C-z" . hydra-meghanada/body)))
+  ;; It's not sufficient for this package to work without this require??
+  (require 'autodisass-java-bytecode))
 
-(defhydra hydra-meghanada (:hint nil :exit t)
-  "
-^Edit^                           ^Tast or Task^
-^^^^^^-------------------------------------------------------
-_f_: meghanada-compile-file      _m_: meghanada-restart
-_c_: meghanada-compile-project   _t_: meghanada-run-task
-_o_: meghanada-optimize-import   _j_: meghanada-run-junit-test-case
-_s_: meghanada-switch-test-case  _J_: meghanada-run-junit-class
-_v_: meghanada-local-variable    _R_: meghanada-run-junit-recent
-_i_: meghanada-import-all        _r_: meghanada-reference
-_g_: magit-status                _T_: meghanada-typeinfo
-_l_: helm-ls-git-ls
-_q_: exit
-"
-  ("f" meghanada-compile-file)
-  ("m" meghanada-restart)
-  ("c" meghanada-compile-project)
-  ("o" meghanada-optimize-import)
-  ("s" meghanada-switch-test-case)
-  ("v" meghanada-local-variable)
-  ("i" meghanada-import-all)
+;; Loosely borrowed from
+;; https://github.com/jmibanez/jmi-dotemacs/blob/master/development.init.el#L139
+;; Other changes from Torstein as well
+;; https://gitlab.com/skybert/my-little-friends/-/blob/master/emacs/.emacs.d/tkj-java.el
+(use-package lsp-java
+  :init
+  (defun kb/java-mode-config ()
+    (subword-mode)
+    (setq-local tab-width 4)
+    (setq-local c-basic-offset 4)
+    (lsp))
 
-  ("g" magit-status)
-  ("l" helm-ls-git-ls)
-  ("t" meghanada-run-task)
-  ("T" meghanada-typeinfo)
-  ("j" meghanada-run-junit-test-case)
-  ("J" meghanada-run-junit-class)
-  ("R" meghanada-run-junit-recent)
-  ("r" meghanada-reference)
+  :config
+  ;; Enable dap-java
+  (require 'dap-java)
 
-  ("q" exit)
-  ("z" nil "leave"))
+  (setq lsp-java-vmargs
+        (list "-noverify"
+              "-Xmx2G"
+              "-XX:+UseG1GC"
+              "-XX:+UseStringDeduplication")
+        lsp-file-watch-ignored '(".idea" ".ensime_cache" ".eunit" "node_modules"
+                                 ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr"
+                                 "_darcs" ".tox" ".svn" ".stack-work" "build")
 
+        lsp-java-save-action-organize-imports nil
+        lsp-enable-on-type-formatting t
+        lsp-enable-indentation t)
+
+  :hook (java-mode  . kb/java-mode-config)
+  :after (lsp lsp-mode dap-mode))
+
+(use-package dap-java
+  :ensure nil
+  :after (lsp-java)
+  :config
+  (dap-register-debug-template
+   "debug..."
+   (list :type "java"
+         :request "attach"
+         :hostName "localhost"
+         :port 5005)))
 
 (provide 'init-java)
 ;;; init-java.el ends here
