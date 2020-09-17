656300f3da88db153d3895c84e86c2833d3b15a8
Kenny Ballou <kballou@devnulllabs.io>
2020-09-17 13:53:27 -0600

    emacs: mpdel: add simple hydra
    Add a simple hydra for various mpdel functions.
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/lisp/init-mpd.el
+++ emacs/.emacs.d/lisp/init-mpd.el
@@ -9,6 +9,25 @@
   :config
   (mpdel-mode))
 
+(defhydra kb/hydra/mpdel (:hint nil :exit t)
+  "
+_SPC_:  Play/Pause
+_s_:    Stop Playback
+_r_:    Randomize playback
+_dr_:   Disable random playback
+_R_:    Repeat placback
+_dR_:   Disable repeat
+"
+  ("q" nil "cancel")
+  ("SPC" libmpdel-playback-play-pause)
+  ("s" libmpdel-stop)
+  ("r" libmpdel-playback-set-random)
+  ("dr" libmpdel-playback-unset-random)
+  ("R" libmpdel-playback-set-repeat)
+  ("dR" libmpdel-playback-unset-repeat))
+
+(define-key global-map (kbd "C-x Z h") #'kb/hydra/mpdel/body)
+
 
 (provide 'init-mpd)
 ;;; init-mpd.el ends here
