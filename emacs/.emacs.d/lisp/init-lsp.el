164cd37871e6235584ce23f3a0bf516a51b31489
Kenny Ballou <kballou@devnulllabs.io>
2020-10-23 15:27:05 -0600

    emacs: lsp: customize lsp and lsp-ui modes
    Many values copied from [[0]].
    
    [0]: https://mullikine.github.io/posts/setting-up-lsp-with-emacs-attempt-2/
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- emacs/.emacs.d/lisp/init-lsp.el
+++ emacs/.emacs.d/lisp/init-lsp.el
@@ -15,27 +15,68 @@
 (defvar lsp-enable-file-watchers)
 (defvar lsp-highlight-symbol-at-point)
 (defvar lsp-prefer-flymake)
+(defvar lsp-log-io)
+(defvar lsp-trace)
+(defvar lsp-print-performance)
+(defvar lsp-auto-guess-root)
+(defvar lsp-document-sync-method)
+(defvar lsp-response-timeout)
+(defvar lsp-clients-go-server-args)
 (defvar lsp-ui-doc-delay)
 (defvar lsp-ui-sideline-enable)
 (defvar lsp-ui-sideline-show-symbol)
+(defvar lsp-ui-imenu-enable)
+(defvar lsp-ui-imenu-kind-position)
+(defvar lsp-ui-flycheck-enable)
+(defvar lsp-ui-peek-enable)
+(defvar lsp-ui-peek-fontify)
+(defvar lsp-ui-peek-list-width)
+(defvar lsp-ui-peek-list-height)
+(defvar lsp-ui-sideline-code-actions-prefix)
+(defvar lsp-ui-sideline-ignore-duplicate)
+(defvar lsp-ui-sideline-show-code-actions)
+(defvar lsp-ui-sideline-show-diagnostics)
+(defvar lsp-ui-sideline-show-hover)
+(defvar lsp-ui-sideline-show-symbol)
+
 
 (use-package lsp-mode
   :config
-  (setq lsp-inhibit-message t
+  (setq lsp-log-io nil
+        lsp-trace nil
+        lsp-print-performance nil
+        lsp-auto-guess-root t
+        lsp-document-sync-method 'incremental
+        lsp-response-timeout 10
+        lsp-prefer-flymake nil
+        lsp-inhibit-message t
         lsp-eldoc-render-all nil
         lsp-enable-file-watchers nil
         lsp-highlight-symbol-at-point nil
         lsp-clients-clangd-args '("-j=4"
                                   "-background-index"
-                                  "-log=error")))
+                                  "-log=error")
+        lsp-clients-go-server-args '("--cache-style=always"
+                                     "--diagnostics-style=onsave")))
 
 (use-package lsp-ui
   :after lsp
   :commands lsp-ui-mode
   :config
-  (setq lsp-prefer-flymake nil
-        lsp-ui-doc-delay 0.3
+  (setq lsp-ui-doc-delay 0.3
         lsp-ui-sideline-enable nil
+        lsp-ui-flycheck-enable t
+        lsp-ui-imenu-enable t
+        lsp-ui-imenu-kind-position 'top
+        lsp-ui-peek-enable t
+        lsp-ui-peek-fontify 'on-demand
+        lsp-ui-peek-list-width 50
+        lsp-ui-peek-list-height 20
+        lsp-ui-sideline-code-actions-prefix ""
+        lsp-ui-sideline-ignore-duplicate t
+        lsp-ui-sideline-show-code-actions nil
+        lsp-ui-sideline-show-diagnostics nil
+        lsp-ui-sideline-show-hover nil
         lsp-ui-sideline-show-symbol nil))
 
 (use-package helm-lsp :after (lsp-mode helm))
