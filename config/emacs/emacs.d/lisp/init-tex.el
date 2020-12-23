a02a7809cfdcfa2065f263916dd5191043fa3aa4
Kenny Ballou <kballou@devnulllabs.io>
2020-12-23 11:07:11 -0700

    emacs: tex: flesh out configuration
    https://tex.stackexchange.com/questions/124501/reftex-multiple-files-problems
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- config/emacs/emacs.d/lisp/init-tex.el
+++ config/emacs/emacs.d/lisp/init-tex.el
@@ -7,17 +7,45 @@
 (defvar TeX-parse-self)
 (defvar TeX-auto-save)
 (defvar TeX-PDF-mode)
+(defvar TeX-save-query)
 (defvar reftex-plug-into-AUCTeX)
 
 (use-package tex
   :hook ((TeX-mode-hook . LaTeX-math-mode)
          (TeX-mode-hook . latex-extra-mode)
          (TeX-mode-hook . turn-on-reftex))
-  :init
+  :config
   (setq TeX-parse-self t
         TeX-auto-save t
-        TeX-PDF-mode t)
-  (setq reftex-plug-into-AUCTeX t))
+        TeX-save-query nil
+        TeX-PDF-mode t
+        LaTeX-eqnarray-label "eq"
+        LaTeX-equation-label "eq"
+        LaTeX-figure-label "fig"
+        LaTeX-table-label "tab"
+        LaTeX-myChapter-label "chap"
+        TeX-newline-function #'reindent-then-newline-and-indent
+        LaTeX-section-hook '(LaTeX-section-heading
+                             LaTeX-section-title
+                             LaTeX-section-toc
+                             LaTeX-section-section
+                             LaTeX-section-label)
+        TeX-source-correlate-method 'synctex
+        TeX-source-correlate-start-server t)
+  (setq-default TeX-master nil))
+
+(use-package tex-site
+  :after tex
+  :config
+  (setq reftex-enable-partial-scans t
+        reftex-save-parse-info t
+        reftex-use-multiple-selection-buffers t
+        reftex-plug-into-AUCTeX t
+        bibtex-entry-format '(opts-or-alts required-fields numerical-fields realign braces)
+        bibtex-align-at-equal-sign t))
+
+(use-package helm-bibtex
+  :after tex-site)
 
 (provide 'init-tex)
 ;;; init-tex.el ends here
