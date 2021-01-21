3142dbded1bdb38a8f39aa482b843007b3981fc6
Kenny Ballou <kballou@devnulllabs.io>
2021-01-21 12:54:56 -0700

    nixpkgs: do not copy overlays
    The overlays as they are do not work outside of the project directory,
    thus, breaking essentially all `nix` commands from the user.
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- config/nixpkgs/default.nix
+++ config/nixpkgs/default.nix
@@ -4,9 +4,4 @@
     source = ./config.nix;
     target = "nixpkgs/config.nix";
   };
-  xdg.configFile.overlays = {
-    recursive = true;
-    source = ../../overlays;
-    target = "nixpkgs/overlays";
-  };
 }
