ff2ac20804dff358607bd36db5528649593cd763
Kenny Ballou <kballou@devnulllabs.io>
2021-02-16 11:10:08 -0700

    fonts: add hack and inconsolata
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- overlays/fonts/default.nix
+++ overlays/fonts/default.nix
@@ -3,15 +3,17 @@ self: super:
   fonts-overlay = super.buildEnv {
     name = "fonts-overlay";
     ignoreCollisions = true;
-    paths = [
-      self.carlito
-      self.dejavu_fonts
-      self.ipafont
-      self.kochi-substitute
-      self.noto-fonts
-      self.source-code-pro
-      self.symbola
-      self.ttf_bitstream_vera
+    paths = with self; [
+      carlito
+      dejavu_fonts
+      hack-font
+      inconsolata-nerdfont
+      ipafont
+      kochi-substitute
+      noto-fonts
+      source-code-pro
+      symbola
+      ttf_bitstream_vera
     ];
   };
 }
