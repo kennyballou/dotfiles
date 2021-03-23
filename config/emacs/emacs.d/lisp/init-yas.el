6f1b6ba23fc0cba8277cfb10620a60c4c51b5da7
Kenny Ballou <kballou@devnulllabs.io>
2021-03-23 11:05:51 -0600

    emacs: yas: hook and reload snippets
    [0]: https://github.com/MatthewZMD/.emacs.d#yasnippet-1
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- config/emacs/emacs.d/lisp/init-yas.el
+++ config/emacs/emacs.d/lisp/init-yas.el
@@ -7,9 +7,11 @@
 
 (use-package yasnippet
   :diminish (yas-minor-mode)
+  :hook ((prog-mode LaTeX-mode org-mode) . yas-minor-mode)
   :config
   (setq yas-snippet-dirs
-        (list (expand-file-name "snippets" user-emacs-directory))))
+        (list (expand-file-name "snippets" user-emacs-directory)))
+  (yas-reload-all))
 
 (provide 'init-yas)
 ;;; init-yas.el ends here
