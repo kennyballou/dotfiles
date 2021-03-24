507dc8c25be868abe78fe885205c64581a62e72e
Kenny Ballou <kballou@devnulllabs.io>
2021-03-23 21:29:51 -0600

    emacs: tex: proper setup of AUCTeX+reftex
    Fix and otherwise integrate AUCTeX+reftex into LaTeX-mode.
    
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- config/emacs/emacs.d/lisp/init-tex.el
+++ config/emacs/emacs.d/lisp/init-tex.el
@@ -13,19 +13,27 @@
 (use-package reftex
   :commands turn-on-reftex
   :config
-  (setq reftex-plug-into-AUCTeX t))
+  (setq reftex-plugin-into-AUCTeX t)
+  (setq reftex-enable-partial-scans t)
+  (setq reftex-save-parse-info t)
+  (setq reftex-use-multiple-selection-buffers t)
+  (setq bibtex-entry-format '(opts-or-alts required-fields numerical-fields realign braces))
+  (setq bibtex-align-at-equal-sign t))
 
-(use-package latex
-  :mode
-  ("\\.tex\\'" . latex-mode)
-  :hook ((TeX-mode-hook . LaTeX-math-mode)
-         (TeX-mode-hook . latex-extra-mode)
-         (TeX-mode-hook . turn-on-reftex))
+(use-package tex
+  :mode ("\\.tex\\'" . LaTeX-mode)
+  :init
+  (defun kb/tex-mode ()
+    "Configure TeX/LaTeX mode settings."
+    (turn-on-reftex)
+    (LaTeX-math-mode t)
+    (reftex-isearch-minor-mode t)
+    (setq TeX-PDF-mode t)
+    (setq TeX-source-correlate-start-server t))
   :config
   (setq TeX-parse-self t)
   (setq TeX-auto-save t)
   (setq TeX-save-query nil)
-  (setq TeX-PDF-mode t)
   (setq LaTeX-eqnarray-label "eq")
   (setq LaTeX-equation-label "eq")
   (setq LaTeX-figure-label "fig")
@@ -37,22 +45,23 @@
                              LaTeX-section-toc
                              LaTeX-section-section
                              LaTeX-section-label))
-  (setq TeX-source-correlate-method 'synctex)
+  (setq TeX-view-program-selection '(((output-dvi has-no-display-manager)
+                                      "dvi2tty")
+                                     ((output-dvi style-pstricks)
+                                      "dvips and gv")
+                                     (output-dvi "xdvi")
+                                     (output-pdf "Evince" "pdf-tools")
+                                     (output-html "xdg-open")))
+  (setq TeX-view-program-list '(("pdf-tools" "TeX-pdf-tools-sync-view")))
+  (setq TeX-source-correlate-method '((dvi . source-specials)
+                                      (pdf . synctex)))
   (setq TeX-source-correlate-start-server t)
-  (setq-default TeX-master nil))
+  (setq-default TeX-master nil)
+  (setq TeX-after-compilation-finished-functions #'TeX-revert-document-buffer)
+  :hook ((LaTeX-mode . kb/tex-mode)
+         (TeX-mode . kb/tex-mode)))
 
-(use-package tex-site
-  :after tex
-  :config
-  (setq reftex-enable-partial-scans t)
-  (setq reftex-save-parse-info t)
-  (setq reftex-use-multiple-selection-buffers t)
-  (setq reftex-plug-into-AUCTeX t)
-  (setq bibtex-entry-format '(opts-or-alts required-fields numerical-fields realign braces))
-  (setq bibtex-align-at-equal-sign t))
-
-(use-package helm-bibtex
-  :after tex-site)
+(use-package helm-bibtex)
 
 (provide 'init-tex)
 ;;; init-tex.el ends here
