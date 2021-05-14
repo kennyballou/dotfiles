50c95aec4c3171cfb4b7f8ae29405fd3a666498f
Kenny Ballou <kb@devnulllabs.io>
2021-05-14 11:45:03 -0600

    mimeapps: update associations -> kde/plasma apps
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- config/mimeapps/default.nix
+++ config/mimeapps/default.nix
@@ -4,22 +4,26 @@
     enable = true;
     associations = {
       added = {
-        "image/gif" = [ "gimp.desktop" "org.gnome.eog.desktop" ];
-        "image/jpeg" = [ "gimp.desktop" "org.gnome.eog.desktop" ];
-        "image/png" = [ "gimp.desktop" "org.gnome.eog.desktop" ];
+        "image/gif" = [ "gimp.desktop" "org.kde.gwenview.desktop" ];
+        "image/jpeg" = [ "gimp.desktop" "org.kde.gwenview.desktop" ];
+        "image/png" = [ "gimp.desktop" "org.kde.gwenview.desktop" ];
       };
       removed = {};
     };
     defaultApplications = {
       "all/text" = [ "emacsclient.desktop" "emacs.desktop" ];
-      "applicaiton/vnd.openxmlformats--officedocument.wordprocessingxml.documentappliation/zip" = [ "libreoffice.desktop" ];
-      "application/jpg"= [ "org.gnome.eog.desktop" "gimp.desktop" ];
-      "application/pdf" = [ "org.gnome.Evince.desktop" ];
+      "applicaiton/vnd.openxmlformats--officedocument.wordprocessingxml.documentappliation/zip" = [
+        "libreoffice.desktop" ];
+      "application/jpg"= [ "org.kde.gwenview.desktop" "gimp.desktop" ];
+      "application/pdf" = [ "org.kde.okular.desktop" ];
+      "application/x-wwf" = [ "org.kde.okular.desktop" ];
+      "application/x-gzpdf" = [ "org.kde.okular.desktop" ];
+      "application/x-bzpdf" = [ "org.kde.okular.desktop" ];
       "application/vnd.ms-excel" = [ "libreoffice.desktop" ];
-      "image/gif" = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-      "image/jpeg" = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-      "image/png" = [ "org.gnome.eog.desktop" "gimp.desktop" ];
-      "inode/directory" = [ "org.Gnome.Nautilus.desktop" ];
+      "image/gif" = [ "org.kde.gwenview.desktop" "gimp.desktop" ];
+      "image/jpeg" = [ "org.kde.gwenview.desktop" "gimp.desktop" ];
+      "image/png" = [ "org.kde.gwenview.desktop" "gimp.desktop" ];
+      "inode/directory" = [ "org.kde.dolphin.desktop" ];
       "text/html" = [ "emacsclient.desktop" "emacs.desktop" "firefox.desktop" "chromium-browser.desktop" ];
       "text/plain" = [ "emacsclient.desktop" "emacs.desktop" ];
       "x-scheme-handler/about" = [ "firefox.desktop" ];
