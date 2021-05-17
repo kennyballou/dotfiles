732d09a99bd6e7b85d7bf789bc1701ace4cc2103
Kenny Ballou <kb@devnulllabs.io>
2021-05-17 10:47:38 -0600

    gtk: add some gtk settings
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- config/gtk/default.nix
+++ config/gtk/default.nix
@@ -13,6 +13,19 @@ in
       name = "Nordic-darker";
       package = pkgs.nordic;
     };
+    font = {
+      name = "DejaVu Sans";
+      package = pkgs.dejavu_fonts;
+      size = 10;
+    };
+    gtk2.extraConfig = ''
+      gtk-enable-animations=1
+      gtk-primary-button-warps-slider=0
+      gtk-toolbar-style=3
+      gtk-menu-images=1
+      gtk-button-images=1
+      gtk-cursor-theme-size=24
+    '';
     gtk3.bookmarks = [
       "file://${home.homeDirectory}/workspace"
       "file://${home.homeDirectory}/workspace/src"
