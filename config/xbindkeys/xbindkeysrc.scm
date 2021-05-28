fcdd10328c4c88b42b1da5b8f0b42a365a571980
Kenny Ballou <kb@devnulllabs.io>
2021-05-28 15:44:50 -0600

    xbindkeys: Add XF86Audio keys to mpc commands
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- config/xbindkeys/xbindkeysrc.scm
+++ config/xbindkeys/xbindkeysrc.scm
@@ -1,2 +1,8 @@
 (xbindkey '(shift "b:4") "xte 'mouseclick 6'")
 (xbindkey '(shift "b:5") "xte 'mouseclick 7'")
+; XF86AudioPlay
+(xbindkey '("m:0x0" "c:172") "mpc toggle")
+; XF86AudioPrev
+(xbindkey '("m:0x0" "c:173") "mpc prev")
+; XF86AudioNext
+(xbindkey '("m:0x0" "c:174") "mpc next")
