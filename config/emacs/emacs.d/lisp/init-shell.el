5c504d56da0171ce229183868b2173d643917f2e
Kenny Ballou <kb@devnulllabs.io>
2021-08-29 23:52:39 -0600

    emacs: shell: bind C-x C-e to something meaningful
    Shell mode does not replace the existing Emacs lisp mode binding of `C-x
    C-e`, causing all sorts of issues.  Add a binding to something more
    meaningful for the mode.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- config/emacs/emacs.d/lisp/init-shell.el
+++ config/emacs/emacs.d/lisp/init-shell.el
@@ -5,12 +5,16 @@
 (defvar bash-completion)
 
 (require 'shell)
+(require 'sh-script)
 
 ;; https://www.masteringemacs.org/article/shell-comint-secrets-history-commands
 (define-key shell-mode-map (kbd "SPC") #'comint-magic-space)
 
 ;; helm comint input ring "history"
 (define-key shell-mode-map (kbd "C-c C-l") #'helm-comint-input-ring)
+
+;; execute region
+(define-key sh-mode-map (kbd "C-x C-e") #'sh-send-line-or-region-and-step)
 
 ;; https://github.com/szermatt/emacs-bash-completion
 (autoload 'bash-completion-dynamic-complete
