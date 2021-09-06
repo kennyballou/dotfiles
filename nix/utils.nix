6833eafe14b67bd9d5daafe79d4c89bbcdc9abb2
Kenny Ballou <kb@devnulllabs.io>
2021-09-06 12:00:37 -0600

    nix: use wiki overrideFlags snippet
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- nix/utils.nix
+++ nix/utils.nix
@@ -1,17 +1,13 @@
 { pkgs, ... }:
 # https://stackoverflow.com/questions/42136197/how-to-override-compile-flags-for-a-single-package-in-nixos
+# https://nixos.wiki/wiki/Snippets
 let
-  overrideWithFlags = pkg: flags:
-    pkgs.lib.overrideDerivation pkg (old:
-      let
-        newflags = pkgs.lib.foldl' (acc: x: "${acc} ${x}") "" flags;
-        oldflags = if (pkgs.lib.hasAttr "NIX_CFLAGS_COMPILE" old)
-                   then "${old.NIX_CFLAGS_COMPILE}"
-                   else "";
-      in
-        {
-          NIX_CFLAGS_COMPILE = "${oldflags} ${newflags}";
-        });
+
+  overrideWithFlag = pkg: flag:
+    pkg.overrideAttrs (attrs: {
+      NIX_CFLAGS_COMPILE = (attrs.NIX_CFLAGS_COMPILE or "") + "${flag}";
+    });
+  overrideWithFlags = pkg: flags: pkgs.lib.foldl' (pkg: flag: overrideWithFlag pkg flag) pkg flags;
 in
 {
   overrideWithFlags = overrideWithFlags;
