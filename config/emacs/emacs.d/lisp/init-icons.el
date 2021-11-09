cf58ca3172b58e752ff1f1459c944f2a0d176bb6
Kenny Ballou <kb@devnulllabs.io>
2021-11-09 13:07:04 -0700

    emacs: disable icons in dired and ibuffer
    Sometimes I want to open folders with _lots_ of files.  Motivation for
    this removal was a folder with 5k files.  The minor benefit of seeing
    some icons on occasion is not worth the 5 to 10 minutes emacs completely
    locks up trying to display some icons.  Spamming ^G and sending SIGUSR2
    is maybe the reason it's _only_ 5 to 10 minutes...
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- config/emacs/emacs.d/lisp/init-icons.el
+++ config/emacs/emacs.d/lisp/init-icons.el
@@ -8,14 +8,6 @@
 
 (use-package all-the-icons)
 
-(use-package all-the-icons-dired
-  :after all-the-icons
-  :hook (dired-mode . all-the-icons-dired-mode))
-
-(use-package all-the-icons-ibuffer
-  :after all-the-icons
-  :hook (ibuffer-mode . all-the-icons-ibuffer-mode))
-
 
 (provide 'init-icons)
 ;;; init-icons.el ends here
