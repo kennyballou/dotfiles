0939c9b04d7b848ed2d8bcf1649e24bf1dfc509e
Kenny Ballou <kb@devnulllabs.io>
2022-01-31 10:10:22 -0700

    guix: initial channels
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

---
+++ config/guix/channels.scm
@@ -0,0 +1,21 @@
+(list (channel
+       (name 'guix)
+       (url "https://git.savannah.gnu.org/git/guix.git")
+       (commit "5b465de06c8d310edb067234769d43c9e7c8d4cc"))
+      (channel
+       (name 'nonguix)
+       (url "https://gitlab.com/nonguix/nonguix")
+       (commit "ca0837cc70d5b4d3fce83b34c31b3378f60f4498"))
+      (channel
+       (name 'rde)
+       (url "https://github.com/abcdw/rde")
+       (commit "4fe2dcbd4a1e8bc35cd639b549f08f72c273c47e"))
+      (channel
+       (name 'flat)
+       (url "https://github.com/flatwhatson/guix-channel.git")
+       (commit "ef4714c105b8c5998f1d3d8db0dab9a461918595"))
+      (channel
+       (name 'emacs)
+       (url "https://github.com/babariviere/guix-emacs.git")
+       (commit "8d45521e2aeac063fba926d375b1a202c5487275")))
+
