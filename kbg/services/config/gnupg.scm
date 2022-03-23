ebaa28d22ab48fd27de2dfdacb5c724d00a16647
Kenny Ballou <kb@devnulllabs.io>
2022-03-23 14:16:06 -0600

    config: gnupg: fix gpg-agent decryption
    Decryption would fail with no pinentry error, generating a new agent
    configuration which includes a reference to a default pinentry program
    seems to resolve the issue.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg/services/config/gnupg.scm
+++ kbg/services/config/gnupg.scm
@@ -2,7 +2,17 @@
   #:use-module (kbg)
   #:use-module (gnu)
   #:use-module (guix)
+  #:use-module (gnu packages gnupg)
   #:use-module (gnu home services))
+
+(define gpg-agent-config
+  (mixed-text-file "gpg-agent.conf"
+                   "enable-ssh-support\n"
+                   "pinentry-program " pinentry-gnome3 "/bin/pinentry-gnome3\n"
+                   "default-cache-ttl 28800\n"
+                   "max-cache-ttl 43200\n"
+                   "allow-emacs-pinentry\n"
+                   "allow-loopback-pinentry\n"))
 
 (define-public gnupg-config-service
   (list (simple-service 'gnupg-config
@@ -10,6 +20,6 @@
                         `(("gnupg/gpg.conf"
                            ,(local-file (string-append %dotfiles-root "config/gnupg/gpg.conf")))
                           ("gnupg/gpg-agent.conf"
-                           ,(local-file (string-append %dotfiles-root "config/gnupg/gpg-agent.conf")))
+                           ,gpg-agent-config)
                           ("gnupg/scdaemon.conf"
                            ,(local-file (string-append %dotfiles-root "config/gnupg/scdaemon.conf")))))))
