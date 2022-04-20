6e04955d5f271f2e27603abe94f70c8c8b0cc503
Kenny Ballou <kb@devnulllabs.io>
2022-04-20 10:42:09 -0600

    config: sort variable keys
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg/services/config/vars.scm
+++ kbg/services/config/vars.scm
@@ -8,8 +8,8 @@
 (define home-vars-service
   (list (simple-service 'home-vars
                         home-environment-variables-service-type
-                        '(("ASPELL_CONF" . "\"per-conf ${XDG_CONFIG_HOME}/aspell/aspell.conf; personal ${XDG_CONFIG_HOME}/aspell/en.pws; repl ${XDG_CONFIG_HOME}/aspell/en.prepl\"")
-                          ("_JAVA_OPTIONS" . "\"-Dawt.useSystemAAFontSettings=on -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel\"")
-                          ("MPD_HOST" . "/run/user/1000/mpd/socket")
+                        '(("_JAVA_OPTIONS" . "\"-Dawt.useSystemAAFontSettings=on -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel\"")
+                          ("ASPELL_CONF" . "\"per-conf ${XDG_CONFIG_HOME}/aspell/aspell.conf; personal ${XDG_CONFIG_HOME}/aspell/en.pws; repl ${XDG_CONFIG_HOME}/aspell/en.prepl\"")
                           ("EDITOR" . "emacsclient")
+                          ("MPD_HOST" . "/run/user/1000/mpd/socket")
                           ("PARALLEL_HOME" . "${XDG_CACHE_HOME}/parallel")))))
