2bc87b869cafe977cd596934f76875a33fca8e90
Kenny Ballou <kb@devnulllabs.io>
2022-06-15 11:57:02 -0600

    config: vars: add GUILE_DRMAA_LIBRARY
    This informs `guix workflow` where to find the DRMAA library for slurm
    when using `drmaa-engine` for `guix workflow` calls.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg/services/config/vars.scm
+++ kbg/services/config/vars.scm
@@ -3,14 +3,16 @@
   #:use-module (gnu)
   #:use-module (guix)
   #:use-module (gnu home services)
+  #:use-module (gnu packages parallel)
   #:export (home-vars-service))
 
 (define home-vars-service
   (list (simple-service 'home-vars
                         home-environment-variables-service-type
-                        '(("_JAVA_OPTIONS" . "\"-Dawt.useSystemAAFontSettings=on -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel\"")
+                        `(("_JAVA_OPTIONS" . "\"-Dawt.useSystemAAFontSettings=on -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel\"")
                           ("ASPELL_CONF" . "\"per-conf ${XDG_CONFIG_HOME}/aspell/aspell.conf; personal ${XDG_CONFIG_HOME}/aspell/en.pws; repl ${XDG_CONFIG_HOME}/aspell/en.prepl\"")
                           ("EDITOR" . "emacsclient")
+                          ("GUILE_DRMAA_LIBRARY" . ,(file-append slurm-drmaa "/lib/libdrmaa.so"))
                           ("GUIX_EXTENSIONS_PATH" . "${GUIX_PROFILE}/share/guix/extensions")
                           ("GUIX_PROFILE" . "${HOME}/.guix-home/profile")
                           ("MPD_HOST" . "/run/user/1000/mpd/socket")
