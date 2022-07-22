e068049726fe9d91283caae3154733229f00fcc8
Kenny Ballou <kb@devnulllabs.io>
2022-07-22 12:53:35 -0600

    kbg: dotfiles-root: use `getcwd`
    Instead of hardcoding path, use the `getcwd` function.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg.scm
+++ kbg.scm
@@ -1,4 +1,4 @@
 (define-module (kbg))
 
 (define-public %home (getenv "HOME"))
-(define-public %dotfiles-root (string-append %home "/workspace/src/io/devnulllabs/kb/dotfiles/"))
+(define-public %dotfiles-root (string-append (getcwd) "/"))
