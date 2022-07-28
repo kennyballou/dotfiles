e0fa6000f055ce816c546881293f0d380e48e8b7
Kenny Ballou <kb@devnulllabs.io>
2022-07-28 13:30:11 -0600

    statistics: add ascii and xtable packages
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg/packages/profiles/statistics.scm
+++ kbg/packages/profiles/statistics.scm
@@ -4,12 +4,15 @@
   #:use-module (gnu packages)
   #:use-module (gnu packages cran)
   #:use-module (gnu packages statistics)
+  #:use-module (kbg packages statistics)
   #:export (%kbg-statistics-packages))
 
 (define %kbg-statistics-packages
   (list
    r
+   r-ascii
    r-dplyr
    r-ggplot2
    r-readr
+   r-xtable
    r-xts))
