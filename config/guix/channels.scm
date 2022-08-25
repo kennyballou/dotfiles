bc9bd92582340ca587088ae7c80b6244d6dcf943
Kenny Ballou <kb@devnulllabs.io>
2022-08-25 11:35:59 -0600

    guix: update channels
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- config/guix/channels.scm
+++ config/guix/channels.scm
@@ -1,11 +1,11 @@
 (list (channel
        (name 'guix)
        (url "https://git.savannah.gnu.org/git/guix.git")
-       (commit "6c2e14a43a86258204e19fe5cbac4a77374bab3f"))
+       (commit "26ff8a4b733c06b4f38aa1fb1ec6705bf30eae4a"))
       (channel
        (name 'nonguix)
        (url "https://gitlab.com/nonguix/nonguix")
-       (commit "af4fc4fb6a2350f7dbb62fbc9479ad77680600e6"))
+       (commit "9cf71573089c878d553314a3a5e51dc7487db449"))
       (channel
        (name 'kbg)
        (url "https://git.sr.ht/~kennyballou/guix-channel.git")
@@ -13,8 +13,8 @@
       (channel
        (name 'flat)
        (url "https://github.com/flatwhatson/guix-channel.git")
-       (commit "d95204cc50de4c3a7abcbff86cfec7d5eed43a7e"))
+       (commit "bfc961929e93114bcbc230edca3a771070efad3e"))
       (channel
        (name 'emacs)
        (url "https://github.com/babariviere/guix-emacs.git")
-       (commit "242b3fbfaacd19c3799d7013379992c1f612246e")))
+       (commit "b0ebcee42cb7b8805d2ae186dbe2f59dbb60f6ac")))
