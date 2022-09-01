e5fc4eccdfe51eb4724c96a9454f1014421f5d83
Kenny Ballou <kb@devnulllabs.io>
2022-08-31 19:07:39 -0600

    config: environment-variables: fix quoting issues
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg/services/config/vars.scm
+++ kbg/services/config/vars.scm
@@ -9,8 +9,8 @@
 (define home-vars-service
   (list (simple-service 'home-vars
                         home-environment-variables-service-type
-                        `(("_JAVA_OPTIONS" . "\"-Dawt.useSystemAAFontSettings=on -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel\"")
-                          ("ASPELL_CONF" . "\"per-conf ${XDG_CONFIG_HOME}/aspell/aspell.conf; personal ${XDG_CONFIG_HOME}/aspell/en.pws; repl ${XDG_CONFIG_HOME}/aspell/en.prepl\"")
+                        `(("_JAVA_OPTIONS" . "-Dawt.useSystemAAFontSettings=on -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel")
+                          ("ASPELL_CONF" . "per-conf ${XDG_CONFIG_HOME}/aspell/aspell.conf; personal ${XDG_CONFIG_HOME}/aspell/en.pws; repl ${XDG_CONFIG_HOME}/aspell/en.prepl")
                           ("EDITOR" . "emacsclient")
                           ("GUILE_DRMAA_LIBRARY" . ,(file-append slurm-drmaa "/lib/libdrmaa.so"))
                           ("GUIX_EXTENSIONS_PATH" . "${GUIX_PROFILE}/share/guix/extensions")
