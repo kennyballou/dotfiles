ae4962a7cd513ddd86b04f46fa6fd423e8412064
Kenny Ballou <kb@devnulllabs.io>
2022-10-17 16:46:58 -0600

    guix: update channels & add introductions
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- config/guix/channels.scm
+++ config/guix/channels.scm
@@ -1,16 +1,45 @@
 (list (channel
-       (name 'guix)
-       (url "https://git.savannah.gnu.org/git/guix.git")
-       (commit "52f82c1af046a1cdb7f2c7f676ae6c80c5baa502"))
-      (channel
-       (name 'nonguix)
-       (url "https://gitlab.com/nonguix/nonguix")
-       (commit "e0951349603581895e0ba61f0e7410368ea1902a"))
+       (name 'emacs)
+       (url "https://github.com/babariviere/guix-emacs.git")
+       (branch "master")
+       (commit
+        "014b733624d3915eee6c62c4e57ff23706c22306")
+       (introduction
+        (make-channel-introduction
+         "8d45521e2aeac063fba926d375b1a202c5487275"
+         (openpgp-fingerprint
+          "261C A284 3452 FB01 F6DF  6CF4 F9B7 864F 2AB4 6F18"))))
       (channel
        (name 'kbg)
        (url "https://git.sr.ht/~kennyballou/guix-channel.git")
-       (commit "2dac0a6ef6d2d4ff45c86dd73059fec6fedbe113"))
+       (branch "master")
+       (commit
+        "0e97479fb93dc173cde8607ca155817f25be2849")
+       (introduction
+        (make-channel-introduction
+         "0e97479fb93dc173cde8607ca155817f25be2849"
+         (openpgp-fingerprint
+          "B74C C4B4 1148 C3DB 364B  C211 82D9 4B35 744E 1B34"))))
       (channel
-       (name 'emacs)
-       (url "https://github.com/babariviere/guix-emacs.git")
-       (commit "c6168c90d4c459c6040505931ab6ffeaa3a8cc0c")))
+       (name 'nonguix)
+       (url "https://gitlab.com/nonguix/nonguix")
+       (branch "master")
+       (commit
+        "4399c012e4bb5460c1807285ed98fb5455816f8c")
+       (introduction
+        (make-channel-introduction
+         "ca0837cc70d5b4d3fce83b34c31b3378f60f4498"
+         (openpgp-fingerprint
+          "8141 6036 E81A 5CF7 8F80  1071 ECFC 8398 8B4E 4B9F"))))
+      (channel
+       (name 'guix)
+       (url "https://git.savannah.gnu.org/git/guix.git")
+       (branch "master")
+       (commit
+        "93d257c3cb23b1b3777d4cecf138dfc37f123f9a")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+
