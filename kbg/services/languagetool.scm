b1edc8021ae277efc860ae25d0cdfe11b64c9b15
Kenny Ballou <kb@devnulllabs.io>
2023-01-17 12:30:54 -0700

    services: languagetool: reorganize LT services
    Split let expression into separate defines.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg/services/languagetool.scm
+++ kbg/services/languagetool.scm
@@ -8,35 +8,41 @@
   #:use-module (gnu home services shepherd)
   #:use-module (gnu packages java)
   #:use-module (kbg packages languagetool)
-  #:use-module (kbg packages ltex-ls)
-  #:export (languagetool-service ltex-service))
+  #:use-module (kbg packages ltex-ls))
 
-(define languagetool-service
-  (let* ((languagetool-server (shepherd-service
-                                (provision '(languagetool))
-                                (requirement '())
-                                (documentation "Run a local LanguageTool Server.")
-                                (start #~(make-forkexec-constructor
-                                          (list #$(file-append openjdk "/bin/java")
-                                                "-classpath"
-                                                #$(file-append languagetool "/languagetool-server.jar")
-                                                "org.languagetool.server.HTTPServer"
-                                                "--port"
-                                                "9090"
-                                                "--languageModel"
-                                                #$(file-append languagetool-ngram-en "/share/LanguageTool-ngrams/"))))
-                                (stop #~(make-kill-destructor))))
-         (ltex-server (shepherd-service
-                       (provision '(ltex-ls))
-                       (requirement '(languagetool))
-                       (documentation "Run TCP LTEX LanguageServer which uses LanguageTool")
-                       (start #~(make-forkexec-constructor
-                                 (list #$(file-append openjdk "/bin/java")
-                                       "-classpath"
-                                       #$(file-append ltex-ls "/lib/*:")
-                                       "org.bsplines.ltexls.LtexLanguageServerLauncher"
-                                       "--host=localhost"
-                                       "--port=9091"
-                                       "--server-type=TcpSocket")))
-                       (stop #~(make-kill-destructor)))))
-    (list languagetool-server ltex-server)))
+(define-public languagetool-shepherd-service
+  (shepherd-service
+   (provision '(languagetool))
+   (requirement '())
+   (documentation "Run a local LanguageTool Server.")
+   (start #~(make-forkexec-constructor
+             (list #$(file-append openjdk "/bin/java")
+                   "-classpath"
+                   #$(file-append languagetool "/languagetool-server.jar")
+                   "org.languagetool.server.HTTPServer"
+                   "--port"
+                   "9090"
+                   "--languageModel"
+                   #$(file-append languagetool-ngram-en "/share/LanguageTool-grams/"))
+             #:log-file ".share/var/log/languagetool.log"))
+   (stop #~(make-kill-destructor))))
+
+(define-public ltex-shepherd-service
+  (shepherd-service
+   (provision '(ltex-ls))
+   (requirement '(languagetool))
+   (documentation "Run TCP LTEX LanguageServer which uses LanguageTool")
+   (start #~(make-forkexec-constructor
+             (list #$(file-append openjdk "/bin/java")
+                   "-classpath"
+                   #$(file-append ltex-ls "/lib/*:")
+                   "org.bsplines.ltexls.LtexLanguageServerLauncher"
+                   "--host=localhost"
+                   "--port=9091"
+                   "--server-type=TcpSocket")))
+   ;; #:log-file ".local/var/log/ltex-ls.log"))
+   (stop #~(make-kill-destructor))))
+
+(define-public languagetool-service
+  (list languagetool-shepherd-service
+        ltex-shepherd-service))
