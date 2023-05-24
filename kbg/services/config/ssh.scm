8d13660ce5c5aecfb6b97142438e64c5c07ca5b1
Kenny Ballou <kb@devnulllabs.io>
2023-05-24 10:40:25 -0600

    config: ssh: do not proxy if reachable
    Use yak as a proxy host for borah.  Selectively proxy through onyx if
    yak is not ICMP reachable.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg/services/config/ssh.scm
+++ kbg/services/config/ssh.scm
@@ -3,12 +3,15 @@
   #:use-module (gnu)
   #:use-module (guix)
   #:use-module (gnu home services)
+  #:use-module (kbg packages networking)
   #:export (ssh-config-service))
 
 (define ssh-config-service
   (list (simple-service 'ssh-config
                         home-files-service-type
-                        `((".ssh/config"
+                        `((".ssh/isup"
+                           ,(file-append isup "/bin/isup"))
+                          (".ssh/config"
                            ,(local-file (string-append %dotfiles-root "config/ssh/ssh/config")))
                           (".ssh/authorized_keys"
                            ,(local-file (string-append %dotfiles-root "config/ssh/ssh/authorized_keys")))
