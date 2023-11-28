0ed5bc46d616676fbc25ba040eb908d25ef1718f
Kenny Ballou <kb@devnulllabs.io>
2023-11-28 12:33:51 -0700

    config: emacs+fonts: fixup emoji/unicode fonts
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg/packages/profiles/fonts.scm
+++ kbg/packages/profiles/fonts.scm
@@ -5,9 +5,13 @@
 
 (define %kbg-fonts
   (list font-adobe-source-code-pro
+        font-awesome
         font-bitstream-vera
         font-dejavu
         font-google-noto
+        font-google-noto-emoji
+        font-google-noto-sans-cjk
+        font-google-noto-serif-cjk
         font-hack
         font-inconsolata
         font-ipa-mj-mincho
