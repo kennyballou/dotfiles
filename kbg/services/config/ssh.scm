93dd91a285a717e2323a4694a62c0df9f8f0047b
Kenny Ballou <kb@devnulllabs.io>
2023-12-05 09:57:03 -0700

    config: ssh: concatenate hosts and base config
    Concatenate all SSH configuration into a single file, symlinked to the
    usual place.
    
    Arbitrary scheme code seems wrong, but this does exactly what we want at
    build time.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg/services/config/ssh.scm
+++ kbg/services/config/ssh.scm
@@ -4,7 +4,17 @@
   #:use-module (guix)
   #:use-module (gnu home services)
   #:use-module (kbg packages networking)
+  #:use-module (ice-9 ftw)
+  #:use-module (ice-9 textual-ports)
   #:export (ssh-config-service))
+
+(define (read-to-string file)
+  (call-with-input-file file get-string-all))
+
+(define ssh-config-files
+  (map (lambda (f) (string-append %dotfiles-root "config/ssh/ssh/config.d/" f))
+       (scandir (string-append %dotfiles-root "config/ssh/ssh/config.d")
+                (lambda (f) (string-contains f ".conf")))))
 
 (define ssh-config-service
   (list (simple-service 'ssh-config
@@ -12,9 +22,11 @@
                         `((".ssh/isup"
                            ,(file-append isup "/bin/isup"))
                           (".ssh/config"
-                           ,(local-file (string-append %dotfiles-root "config/ssh/ssh/config")))
+                           ,(plain-file "kb-ssh-config"
+                                        (string-join (append (map read-to-string ssh-config-files)
+                                                             (list (read-to-string (string-append
+                                                                                    %dotfiles-root
+                                                                                    "config/ssh/ssh/config"))))
+                                                             "\n")))
                           (".ssh/authorized_keys"
-                           ,(local-file (string-append %dotfiles-root "config/ssh/ssh/authorized_keys")))
-                          (".ssh/config.d"
-                           ,(local-file (string-append %dotfiles-root "config/ssh/ssh/config.d")
-                                        #:recursive? #t))))))
+                           ,(local-file (string-append %dotfiles-root "config/ssh/ssh/authorized_keys")))))))
