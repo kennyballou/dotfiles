de4c172d63ac889e73cdafac7bf59f6d93075c6d
Kenny Ballou <kb@devnulllabs.io>
2023-12-20 09:16:18 -0700

    guix: update channels
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- config/guix/channels.scm
+++ config/guix/channels.scm
@@ -5,7 +5,7 @@
        (url "https://git.sr.ht/~kennyballou/guix.git")
        (branch "master")
        (commit
-        "d5298c5e334e56a9aabddcb62d312e63135864f6")
+        "2b12c1238c802076ddc5d272680a1bb7df4c99c1")
        (introduction
         (make-channel-introduction
          "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -16,7 +16,7 @@
        (url "https://gitlab.com/nonguix/nonguix")
        (branch "master")
        (commit
-        "25b3a7b6f6ed6aa78c30ea3ea51828c15b729bcb")
+        "5d308347ca094d045dcc932540262f5f60c63eed")
        (introduction
         (make-channel-introduction
          "ca0837cc70d5b4d3fce83b34c31b3378f60f4498"
@@ -27,7 +27,7 @@
        (url "https://git.sr.ht/~kennyballou/guix-channel.git")
        (branch "master")
        (commit
-        "e612000a420c101549481217a48de01ac11886ea")
+        "5dbe9f1b954e3c406d40980d7d46d1364fbc36e1")
        (introduction
         (make-channel-introduction
          "b9d0b8041d28ebd9f85cb041aa3f2235c8b39417"
@@ -38,7 +38,7 @@
        (url "https://github.com/babariviere/guix-emacs.git")
        (branch "master")
        (commit
-        "52c84885ed6c657743acb5d4e581010b6786076c")
+        "92b8b9314c302f11bc73d9fa4a0fc8377602314e")
        (introduction
         (make-channel-introduction
          "8d45521e2aeac063fba926d375b1a202c5487275"
