9380f4fff0d96c980dc79f9e36e48ddbbe59c4e5
Kenny Ballou <kb@devnulllabs.io>
2023-12-21 16:24:20 -0700

    fonts: add some nerd fonts
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg/packages/profiles/fonts.scm
+++ kbg/packages/profiles/fonts.scm
@@ -1,4 +1,5 @@
 (define-module (kbg packages profiles fonts)
+  #:use-module (kbg packages fonts)
   #:use-module (gnu packages fonts))
 
 (export %kbg-fonts)
@@ -8,12 +9,13 @@
         font-awesome
         font-bitstream-vera
         font-dejavu
+        font-dejavu-nerd
         font-google-noto
         font-google-noto-emoji
         font-google-noto-sans-cjk
         font-google-noto-serif-cjk
         font-hack
+        font-hack-nerd
         font-inconsolata
         font-ipa-mj-mincho
         font-liberation))
-
