6da2e86b40a39e9247dffcbce8f8d151f62ec46c
Kenny Ballou <kb@devnulllabs.io>
2025-04-25 15:45:01 -0700

    profiles: statistics: remove r-ascii
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg/packages/profiles/statistics.scm
+++ kbg/packages/profiles/statistics.scm
@@ -4,13 +4,11 @@
   #:use-module (gnu packages)
   #:use-module (gnu packages cran)
   #:use-module (gnu packages statistics)
-  #:use-module (kbg packages statistics)
   #:export (%kbg-statistics-packages))
 
 (define %kbg-statistics-packages
   (list
    r
-   r-ascii
    r-dplyr
    r-ggplot2
    r-readr
