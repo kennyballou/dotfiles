5c34880de5892943db0958ffa3105420947ccbf1
Kenny Ballou <kb@devnulllabs.io>
2025-09-05 08:21:29 -0700

    profiles: remove battop
    Crates have been culled.
    
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg/packages/profiles/laptop.scm
+++ kbg/packages/profiles/laptop.scm
@@ -1,8 +1,6 @@
 (define-module (kbg packages profiles laptop)
   #:use-module (gnu packages linux)
-  #:use-module (kbg packages crates-io)
   #:export (%kbg-laptop-packages))
 
 (define %kbg-laptop-packages
-  (list rust-battop-0.2
-        bolt))
+  (list bolt))
