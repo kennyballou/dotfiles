ad3e519a6e78ba2ad98f28a5c320070f1dfdfd9c
Kenny Ballou <kb@devnulllabs.io>
2025-12-11 07:30:55 -0800

    guix: channels: update
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg/channels.scm
+++ kbg/channels.scm
@@ -8,7 +8,7 @@
          (url "https://codeberg.org/guix/guix")
          (branch "master")
          (commit
-          "94b6528cbda6839c0f825a006a86071abcb32c70")
+          "21f0be1aa341dd2cb6722a069375030e714fc046")
          (introduction
           (make-channel-introduction
            "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -19,7 +19,7 @@
          (url "https://gitlab.com/nonguix/nonguix")
          (branch "master")
          (commit
-          "ba6ce23e785d1ccd9194202d8f27f6db4b649170")
+          "53c477b3e0a45b3bd0036647ac805a8f5e8f71d0")
          (introduction
           (make-channel-introduction
            "ca0837cc70d5b4d3fce83b34c31b3378f60f4498"
@@ -30,7 +30,7 @@
          (url "https://git.sr.ht/~kennyballou/guix-channel.git")
          (branch "trunk")
          (commit
-          "2c70c07218a982c87aae466c37ff485bfbe6dce2")
+          "d52068de2c5b1e6570a7f91ce34301ab2a9be03e")
          (introduction
           (make-channel-introduction
            "b9d0b8041d28ebd9f85cb041aa3f2235c8b39417"
